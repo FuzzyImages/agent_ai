3dfb8f08f6a46d720121e2d5448eef9330b97611
FuzzyImages <mursa.artdragons@gmail.com>
2025-06-12 22:16:39 -0700

    created and tested get_file_content.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", ".") + "\n")
-print(get_files_info("calculator", "pkg") + "\n")
-print(get_files_info("calculator", "/bin") + "\n")
-print(get_files_info("calculator", "../") + "\n")
+print(get_file_content("calculator", "main.py") + "\n")
+print(get_file_content("calculator", "pkg/calculator.py") + "\n")
+print(get_file_content("calculator", "/bin/cat") + "\n")
